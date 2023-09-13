7b25de91b79e741c4afbe34edc2a15ff3694af53
carles <galan.carles@gmail.com>
2023-09-13 13:56:01 +0200

    Apply styles to RegisterPage component

---
+++ src/pages/RegisterPage/RegisterPageStyled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const RegisterPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 140px;
+  gap: 20px;
+
+  .register-page {
+    &__title {
+      text-align: center;
+      text-transform: uppercase;
+      width: 100%;
+    }
+  }
+`;
+
+export default RegisterPageStyled;
